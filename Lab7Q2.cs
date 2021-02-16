7ff14c628a338a1cb48561f41880f8d41f0a329e
Kushanthkushi <kushanthkushi99@gmail.com>
2021-02-16 15:49:09 +0530

    intial

---
+++ Lab7Q2.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabBook7
+{
+    class Lab7Q2
+    {
+        ArrayList al = new ArrayList();
+        public static void ALAdd()
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Enter the Element to add");
+            string add1 = Console.ReadLine();
+            al.Add(add1);
+        }
+        public static void ALSearch()
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Enter the Element to search");
+            string search = Console.ReadLine();
+            for (int i = 0; i < al.Count; i++)
+            {
+                if (al.Equals(search))
+                {
+                    Console.WriteLine(search);
+                }
+            }
+        }
+        public static void ALRem()
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine("Enter the Element to delete");
+            string rem = Console.ReadLine();
+            al.Remove(rem);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Add Product" +
+                "2.Delete" +
+                "3.Search" +
+                "4.Save");
+            string c = Console.ReadLine();
+            /*switch(c)
+            {
+                //case 1:
+                    ALAdd();
+               // case 2:
+                    ALRem();
+                //case 3:
+                    ALSearch();
+                //default:
+                    //Console.WriteLine('Enter the valid Option');
+            }*/
+        ALAdd();
+        ALRem();
+        ALSearch();
+    }
+    }
+}
---
+++ Lab8Q1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LabBook8
+{
+    class Lab8Q1
+    {
+        Hashtable ht = new Hashtable();
+        public static void HashAdd()
+        {
+            Hashtable ht = new Hashtable();
+            Console.WriteLine("Enter the Element to add");
+            string add1 = Console.ReadLine();
+            string add2 = Console.ReadLine();
+            ht.Add(add1, add2);
+        }
+        public static void HashSearch()
+        {
+            Hashtable ht = new Hashtable();
+            Console.WriteLine("Enter the Element to search");
+            string search = Console.ReadLine();
+            ht.ContainsKey(search);
+        }
+        public static void Hashdis()
+        {
+            Hashtable ht = new Hashtable();
+            ICollection key = ht.Keys;
+            foreach (var k in key)
+            {
+                Console.WriteLine(k + ":" + ht[k]);
+            }
+        }
+        public static void HashCount()
+        {
+            Hashtable ht = new Hashtable();
+            Console.WriteLine("Total number of elements present"+ ht.Count);
+        }
+        public static void HashRem()
+        {
+            Hashtable ht = new Hashtable();
+            Console.WriteLine("Enter the Element to delete");
+            string rem = Console.ReadLine();
+            ht.Remove(rem);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1.Add Record in Hashtable" +
+                "2.Search record" +
+                "3.Display All Records" +
+                "4.To display Total count of Records at any point" +
+                "5.Remove any particular record");
+            string c = Console.ReadLine();
+            HashAdd();
+            HashSearch();
+            HashCount();
+            Hashdis();
+            HashRem();
+        }
+    }
+}
